39eb37023a4620aea8c65073f04844b587e8e39f
roohankhan8 <roohank8@gmail.com>
2022-10-22 02:58:46 +0500

    First

--- clock.py
+++ clock.py
@@ -1,22 +1,22 @@
-# import imp
-# from tk import *
-# from tk import font
-# from tk.ttk import *
+import imp
+from tk import *
+from tk import font
+from tk.ttk import *
 
-# from time import strftime
+from time import strftime
 
-# root = Tk()
-# root.title = 'Clock'
+root = Tk()
+root.title = 'Clock'
 
-# def time():
-#     string = strftime('%H:%M:%S %p')
-#     label.config(text=string)
-#     label.after(1000, time)
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
 
 
-# label = Label(root, foreground='cyan',background='black')
-# label.pack(anchor='center')
+label = Label(root, foreground='cyan',background='black')
+label.pack(anchor='center')
 
-# time()
+time()
 
-# mainloop()
+mainloop()
--- weather.py
+++ weather.py
@@ -1,53 +1,53 @@
-# import json
-# from logging import root
-# from tkinter import *
-# import tkinter as tk
-# import requests
-# import time
+import json
+from logging import root
+from tkinter import *
+import tkinter as tk
+import requests
+import time
 
 # root = Tk()
 
-# def getWeather(canvas):
-#     city = textfield.get()
-#     api = 'https://api.openweathermap.org/data/2.5/weather?q=' + str(city) + '&appid=91026a32e20292e7883094431a6b1d5a'
-#     json_data = requests.get(api).json()
-#     condition = json_data['weather'][0]['main']
-#     temp = int(json_data['main']['temp'] - 273.15)
-#     feels = int(json_data['main']['"feels_like"'] - 273.15)
-#     max_temp = int(json_data['main']['temp_min'] - 273.15)
-#     min_temp = int(json_data['main']['temp_min'] - 273.15)
-#     sunrise = time.strftime('%I:%M:%S', time.gmtime(
-#         json_data['sys']['sunrise'] - 21600))
-#     sunset = time.strftime('%I:%M:%S', time.gmtime(
-#         json_data['sys']['sunset'] - 21600))
+def getWeather(canvas):
+    city = textfield.get()
+    api = 'https://api.openweathermap.org/data/2.5/weather?q=' + str(city) + '&appid=91026a32e20292e7883094431a6b1d5a'
+    json_data = requests.get(api).json()
+    condition = json_data['weather'][0]['main']
+    temp = int(json_data['main']['temp'] - 273.15)
+    feels = int(json_data['main']['"feels_like"'] - 273.15)
+    max_temp = int(json_data['main']['temp_min'] - 273.15)
+    min_temp = int(json_data['main']['temp_min'] - 273.15)
+    sunrise = time.strftime('%I:%M:%S', time.gmtime(
+        json_data['sys']['sunrise'] - 21600))
+    sunset = time.strftime('%I:%M:%S', time.gmtime(
+        json_data['sys']['sunset'] - 21600))
 
-#     final_info = condition + '\n' + str(temp) + '°C'
-#     final_data = '\n' + 'Max Temp: ' + str(max_temp) + '\n' + 'Min Temp: ' + str(
-#         min_temp) + '\n' + 'Sunrise: ' + str(sunrise) + '\n' + 'Sunset: ' + str(sunset)
+    final_info = condition + '\n' + str(temp) + '°C'
+    final_data = '\n' + 'Max Temp: ' + str(max_temp) + '\n' + 'Min Temp: ' + str(
+        min_temp) + '\n' + 'Sunrise: ' + str(sunrise) + '\n' + 'Sunset: ' + str(sunset)
 
-#     label1.config(text=final_info)
-#     label2.config(text=final_data)
+    label1.config(text=final_info)
+    label2.config(text=final_data)
 
-#     print(final_info)
-#     print(final_data)
+    # print(final_info)
+    # print(final_data)
 
-#     print(city)
+    # print(city)
 
-# canvas = tk.Tk()
-# canvas.geometry("600x600")
-# canvas.title('Weather App')
+canvas = tk.Tk()
+canvas.geometry("600x600")
+canvas.title('Weather App')
 
-# f = ('poppins', 15, 'bold')
-# t = ('poppins', 35, 'bold')
+f = ('poppins', 15, 'bold')
+t = ('poppins', 35, 'bold')
 
-# textfield = Entry(canvas, font=t)
-# textfield.pack(pady=20)
-# textfield.focus()
-# textfield.bind('<Return>', getWeather)
+textfield = Entry(canvas, font=t)
+textfield.pack(pady=20)
+textfield.focus()
+textfield.bind('<Return>', getWeather)
 
-# label1 = tk.Label(canvas, font=t)
-# label1.pack()
-# label2 = tk.Label(canvas, font=f)
-# label2.pack()
+label1 = tk.Label(canvas, font=t)
+label1.pack()
+label2 = tk.Label(canvas, font=f)
+label2.pack()
 
-# canvas.mainloop()
+canvas.mainloop()
